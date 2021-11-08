3982679de7da83e19fa597b70e4d34c37913688e
Wael Ben Mansour <Benmansourwael@live.fr>
2021-11-08 19:14:33 +0100

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/delta-digital-design'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/delta-digital-design/index.html'));});
+app.get('*', (req, res) => {
+  res.sendFile(`/dist/delta-digital-design/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+});
 app.listen(process.env.PORT || 8080);
